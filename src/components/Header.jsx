2c2e60493c22ad1b0b175e65ab5a2d6d5ed4a0d8
WhoAmI471 <artglad05@mail.ru>
2023-02-23 18:50:55 +0500

    menu + baner

---
+++ src/components/Header.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import logo from './img/Logo.svg'
+import serch from './img/serch.svg'
+import basket from './img/basket.svg'
+import user from './img/user.svg'
+import './header.css';
+
+const Header = () => {
+    return (
+        <header>
+            <div className="logo">
+                <a href="/">
+                    <img src={logo} width="106" height="27" alt=''/>
+                </a>
+            </div>
+            <div className="menu">
+                <ul>
+                    <li><a href="/">home</a></li>
+                    <li><a href="/">about</a></li>
+                    <li><a href="/">product</a></li>
+                    <li><a href="/">blog</a></li>
+                    <li><a href="/">shop</a></li>
+                    <li><a href="/">contact us</a></li>
+                </ul>
+            </div>
+            <div className="more">
+                <a href="/">
+                    <img src={serch} width="16" height="16" alt=''/>
+                </a>
+                <a href="/">
+                    <img src={basket} width="27" height="20.27" alt=''/>
+                </a>
+                <a href="/">
+                    <img src={user} width="35" height="35" alt=''/>
+                </a>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/HeroArea.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './HeroArea.css';
+
+const HeroArea = () => {
+    return (
+        <div className='area'>
+            <div className="info">
+                <p className='altName'>Favorite brands</p>
+                <h1><cvet>An exciting place</cvet><br /> 
+                    for the whole family <br />
+                    to shop.</h1>
+                <p>The standard chunk of Lorem Ipsum used since the 1500s <br /> is reproduced below for those interested.</p>
+            </div>
+        </div>
+    );
+}
+
+export default HeroArea;
